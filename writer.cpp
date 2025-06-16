295eeb71f03db427f5b68f8842fb77f911f43853
HenL188 <153272326+HenL188@users.noreply.github.com>
2025-06-15 23:49:35 -0600

    start of writer

--- writer.cpp
+++ writer.cpp
@@ -7,7 +7,10 @@ void writer::function_write(lexer &lex) {
     for (int i = 0; i < lex.fun_name.size(); i++) {
         if (lex.return_type[i] == "void") {
             main << lex.return_type[i] << " " << lex.fun_name[i]<< "(" << lex.fun_args[i]
-            << ")" << " " << "{" << std::endl << "\t" << ";" << " " << "}" << std::endl;
+            << ")" << " " << "{" << std::endl;
+            for (int i = 0; i < lex.var_name.size(); i++) {
+                main << "\t" << ";" << std::endl << " " << "}" << std::endl << lex.var_type[i] << " " << lex.var_name[i] << " " << "=" << " " << lex.var_value[i];
+            }
         }
         else {
             main << lex.return_type[i] << " " << lex.fun_name[i]<< "(" << lex.fun_args[i]
